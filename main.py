9b32e02dc4dd1d8500f092cae6ff3c6a70fa0886
rangashree <90241101+rangashree@users.noreply.github.com>
2022-07-10 20:59:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+
+words = ['cat', 'dog', 'why', 'what', 'this', 'stupid', 'idiot', 'man',
+         'women', 'ink', 'blue', 'green', 'yellow', 'black', 'yes', 'no',
+         'when', 'will']
+hard_words = ['elephant', 'google', 'missing',
+              'statement', 'white', 'greeter']
+
+
+import random
+
+
+def word_game():
+    answer = random.choice(words)
+    num_wrong_guess_left = 6
+    guessed_letter = []
+
+    while num_wrong_guess_left > 0:
+        display_word = ""
+        for letter in answer:
+            if letter in guessed_letter:
+                display_word += letter
+            else:
+                display_word += '_'
+            display_word += ' '
+        print(display_word.strip())
+        print(f'{num_wrong_guess_left} wrong guess left')
+        print(f'guesses: {guessed_letter}')
+
+        guess = input("guessed letter")
+        guessed_letter.append(guess)
+
+        if guess in answer:
+            print("correct")
+        else:
+            print("nope")
+            num_wrong_guess_left -= 1
+
+        won = True
+        for letter in answer:
+            if letter not in guessed_letter:
+                won = False
+                break
+        if won:
+            print("you win !")
+            return
+    print("sorry you lose")
+    print(f'the answer was {answer}')
+
+
+word_game()
